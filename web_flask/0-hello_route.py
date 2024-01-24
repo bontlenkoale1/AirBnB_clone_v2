1929937845bf360858c60d27879a29f3cbb84a10
bontlenkoale1 <bontlenkoale20@gmail.com>
2024-01-24 13:41:59 +0000

    update

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application """
-from flask import Flask
-app = Flask(__name__)
+""" Starts a Flash Web Application
+Your Web Application must be listening on 0.0.0.0, port 5000
+"""
+from flask import Flask, render_template
+app = Flask("__name__")
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """ Prints a Message when / is called """
-    return 'Hello HBNB!'
+def hello():
+    """ Return a given string """
+    return render_template("10-hbnb_filters.html")
 
 
 if __name__ == "__main__":
-    """ Main Function """
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=None)
